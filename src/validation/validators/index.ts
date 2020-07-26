73fa60fb0fa6aa40563f756d2fe1ba4a9d8a2c6b
Fellipe Botelho <fellipegpbotelho@gmail.com>
2020-07-26 11:17:38 -0300

    feat: add design pattern builder to validation

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,4 @@
+export * from './email/email-validation'
+export * from './min-length/min-lenght-validation'
+export * from './required-field/required-field-validation'
+export * from './validation-composite/validation-composite'
